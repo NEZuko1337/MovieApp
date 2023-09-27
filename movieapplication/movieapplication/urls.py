d1c081dfb48aa5464f9a6d643727fdf2748830c1
Zuko1337 <zuko13371@yandex.ru>
2023-09-27 17:30:54 +0300

    Добавил функции отображения в urls

--- movieapplication/movieapplication/urls.py
+++ movieapplication/movieapplication/urls.py
@@ -2,9 +2,12 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls.static import static
+from movieapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('advice/', views.advice_film, name = 'advice')
 ]
 
 if settings.DEBUG:
