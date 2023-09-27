962a54cbb03441c5b55de1b1aedbe0e5c6accdcd
Zuko1337 <zuko13371@yandex.ru>
2023-09-27 17:32:33 +0300

    Обновил вьюху

--- movieapplication/movieapp/views.py
+++ movieapplication/movieapp/views.py
@@ -1,2 +1,9 @@
 from django.shortcuts import render
 
+
+def home(request):
+    return render(request, 'movieapp/home.html')
+
+
+def advice_film(request):
+    return render(request, 'movieapp/advice_film.html')
