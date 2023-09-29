d377a9f407ef0eed75da9b7785effd971b580a2f
Zuko1337 <zuko13371@yandex.ru>
2023-09-29 15:15:23 +0300

    Обновил urls

--- movieapplication/movieapplication/urls.py
+++ movieapplication/movieapplication/urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls.static import static
 from movieapp import views
 
 urlpatterns = [
@@ -10,6 +10,6 @@ urlpatterns = [
     path('advice', views.advice_film, name = 'advicepage'),
     path('advice/', views.advice_film, name = 'advice'),
 ]
-
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
