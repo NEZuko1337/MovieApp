67455a4b0b3fcc0804b751455d79dca277238e41
Zuko1337 <zuko13371@yandex.ru>
2023-09-29 15:14:18 +0300

    Создал модельку с фильмами

--- movieapplication/movieapp/models.py
+++ movieapplication/movieapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Films(models.Model):
+    title = models.CharField(max_length=128, null=False)
+    description = models.TextField(max_length=512, null = False)
+    genre = models.CharField(max_length=32, null = False)
+    photo = models.ImageField(null = False)
+    actors = models.TextField(max_length=1024, blank=True)
+
+
+    def __str__(self) -> str:
+        return f'{self.title} | {self.genre}'
