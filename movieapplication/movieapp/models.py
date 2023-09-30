99ced5dcdb2ec52712f4b2e9b5ab9f8b1744c89c
Zuko1337 <zuko13371@yandex.ru>
2023-09-30 11:45:56 +0300

    Моделька с фильмами

--- movieapplication/movieapp/models.py
+++ movieapplication/movieapp/models.py
@@ -4,7 +4,7 @@ class Films(models.Model):
     title = models.CharField(max_length=128, null=False)
     description = models.TextField(max_length=512, null = False)
     genre = models.CharField(max_length=32, null = False)
-    photo = models.ImageField(null = False)
+    photo = models.ImageField(null = False, upload_to='films_page')
     actors = models.TextField(max_length=1024, blank=True)
 
 
